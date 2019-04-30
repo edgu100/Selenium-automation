4e1acb57e1bcf3aff9e91a70e01685ffc14a1626
edgu1 <edgugold@gmail.com>
2019-05-01 02:30:45 +1200

    添加项目文件。

---
+++ Selenium automation/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium_automation
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.google.com");
+            driver.FindElement(By.Id("tophf")).SendKeys("Selenium automation");
+            driver.FindElement(By.Id("tophf")).SendKeys(Keys.Enter);
+        }
+    }
+}
